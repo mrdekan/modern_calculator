58b9da76f3cfd3573c78c9104ce44b87df1800eb
mrdekan <yeros05040504@gmail.com>
2023-05-05 11:18:44 +0300

    Project created

---
+++ modern_calculator/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace modern_calculator
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Close_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void Hide_Click(object sender, RoutedEventArgs e)
+		{
+			this.WindowState = WindowState.Minimized;
+		}
+
+		private void Title_MouseDown_1(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left) this.DragMove();
+		}
+
+		private void One_Click(object sender, RoutedEventArgs e)
+		{
+			if (Result_calc.Text == "0")
+			{
+				Result_calc.Text = "1";
+				return;
+			}
+			Result_calc.Text += "1";
+		}
+	}
+}
