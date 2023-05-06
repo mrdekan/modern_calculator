b356614041f7467363e7d28f93f1da83cb0b1358
mrdekan <yeros05040504@gmail.com>
2023-05-06 14:02:47 +0300

    Completed translation between different number systems

---
+++ modern_calculator/Core/Converter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modern_calculator.Core
+{
+	internal class Converter
+	{
+		private const int AsciiZero = 48;
+		private const int AsciiCapitalA = 65;
+		private const int AsciiTrivialA = 97;
+		private const int LettersBegin = 10;
+		private const int LettersCount = 6;
+		private int getValueFromHexadecimalNumber(char num)
+		{
+			if ((int)num < AsciiZero + LettersBegin)
+				return (int)num - AsciiZero;
+			else if ((int)num < AsciiCapitalA + LettersCount)
+				return (int)num - (AsciiCapitalA - LettersBegin);
+			return (int)num - (AsciiTrivialA - LettersBegin);
+		}
+		private char getInHexadecimalSystem(int num)
+		{
+			if (num <= 9)
+				return num.ToString()[0];
+			return (char)(num + AsciiCapitalA - LettersBegin);
+		}
+		public string Convert(int from, int to, string input, int afterPoint)
+		{
+			if (from == to)
+				return input;
+			if (to == 10)
+				return toDecimal(from, input);
+			return fromDecimal(to, toDecimal(from, input), afterPoint);
+		}
+		private string fromDecimal(int resNumSystem, string input, int numsAfterPoint)
+		{
+			StringBuilder resStr = new StringBuilder();
+			StringBuilder before = new StringBuilder();
+			foreach (char c in input)
+			{
+				if (c == ',') break;
+				before.Append(c);
+			}
+			int numBefore = int.Parse(before.ToString());
+			while (numBefore >= resNumSystem)
+			{
+				resStr.Append(getInHexadecimalSystem(numBefore % resNumSystem));
+				numBefore = (numBefore - numBefore % resNumSystem) / resNumSystem;
+			}
+			resStr.Append(getInHexadecimalSystem(numBefore));
+			string temp = new string(resStr.ToString().Reverse().ToArray());
+			resStr.Clear();
+			resStr.Append(temp);
+			if (before.ToString() != input)
+			{
+				resStr.Append(',');
+				StringBuilder after = new StringBuilder();
+				after.Append("0,");
+				for (int i = before.Length + 1; i < input.Length; i++) after.Append(input[i]);
+				double numAfter = double.Parse(after.ToString());
+				for (int i = 0; i < numsAfterPoint; i++)
+				{
+					numAfter *= resNumSystem;
+					while (numAfter >= resNumSystem) numAfter -= resNumSystem;
+					resStr.Append(getInHexadecimalSystem((int)Math.Floor(numAfter)));
+				}
+			}
+			return resStr.ToString();
+		}
+		private string toDecimal(int numSystem, string value)
+		{
+			double res = 0;
+			int startPos = value.Contains(',') ? Array.IndexOf(value.ToCharArray(), ',') : value.Length;
+			for (int i = 0, j = 0; i < value.Length; i++, j++)
+			{
+				if (value[i] == ',')
+				{
+					j--;
+					continue;
+				}
+				res += Math.Pow(numSystem, startPos - 1 - j) * getValueFromHexadecimalNumber(value[i]);
+			}
+			return res.ToString();
+		}
+	}
+}
--- modern_calculator/MVVM/View/NumSysView.xaml.cs
+++ modern_calculator/MVVM/View/NumSysView.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using modern_calculator.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,9 +23,14 @@ namespace modern_calculator.MVVM.View
 	public partial class NumSysView : UserControl
 	{
 		private int inputNumSys;
+		private Converter converter = new Converter();
+		private int[] numSystems = { 2, 8, 10, 16 };
 		public NumSysView()
 		{
 			InitializeComponent();
+			FromSys_NumSys.SelectedIndex = 2;
+			ToSys_NumSys.SelectedIndex = 0;
+			ErrorBorder_NumSys.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
 		}
 		private static bool HasNotMoreOneComma(string s)
 		{
@@ -31,17 +38,80 @@ namespace modern_calculator.MVVM.View
 			foreach(char c in s) if(c == ',' || c == '.') count++;
 			return count < 1;
 		}
+		private void ClearError()
+		{
+			ErrorLabel_NumSys.Text = "";
+			ErrorBorder_NumSys.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+		}
+		private void Error(string err)
+		{
+			ErrorBorder_NumSys.Background = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+			ErrorLabel_NumSys.Text = err;
+		}
+		private bool CheckInput(int numSys, string input)
+		{
+			switch (numSys)
+			{
+				case 0:
+					return Regex.IsMatch(input, "^[0-1,]+$");
+				case 1:
+					return Regex.IsMatch(input, "^[0-7,]+$");
+				case 2:
+					return Regex.IsMatch(input, "^[0-9,]+$");
+				case 3:
+					return Regex.IsMatch(input, "^[0-9A-Fa-f,]+$");
+			}
+			return false;
+		}
 		private void NumSys_input_KeyDown(object sender, KeyEventArgs e)
 		{
 			if(e.Key == Key.OemComma || e.Key == Key.OemPeriod)
 			{
-				if(NumSys_input.Text == "" || !HasNotMoreOneComma(NumSys_input.Text)) e.Handled = true;
+				if(!HasNotMoreOneComma(NumSys_input.Text)) e.Handled = true;
 				return;
 			}
-			if (e.Key < Key.D0 || e.Key > Key.D9 || NumSys_input.Text.Length > 26)
+			/*if (e.Key < Key.D0 || e.Key > Key.D9 || NumSys_input.Text.Length > 26)
 			{
 				e.Handled = true;
+			}*/
+		}
+
+		private void Reverse_NumSys_Click(object sender, RoutedEventArgs e)
+		{
+			ClearError();
+			if (NumSys_input.Text != "")
+			{
+				NumSys_input.Text = converter.Convert(numSystems[FromSys_NumSys.SelectedIndex], numSystems[ToSys_NumSys.SelectedIndex], NumSys_input.Text, 8);
+			}
+			int temp = FromSys_NumSys.SelectedIndex;
+			FromSys_NumSys.SelectedIndex = ToSys_NumSys.SelectedIndex;
+			ToSys_NumSys.SelectedIndex = temp;
+			if (NumSys_input.Text != "")
+			{
+				NumSys_output.Text = converter.Convert(numSystems[FromSys_NumSys.SelectedIndex], numSystems[ToSys_NumSys.SelectedIndex], NumSys_input.Text, 8);
 			}
 		}
-    }
+
+		private void Submit_NumSys_Click(object sender, RoutedEventArgs e)
+		{
+			ClearError();
+			NumSys_input.Text = NumSys_input.Text.Replace(".", ",").ToUpper();
+			if (NumSys_input.Text == "")
+			{
+				Error("This field can not be empty");
+				return;
+			}
+			if(!CheckInput(FromSys_NumSys.SelectedIndex, NumSys_input.Text))
+			{
+				Error("Incorrect input");
+				return;
+			}
+			NumSys_output.Text = converter.Convert(numSystems[FromSys_NumSys.SelectedIndex], numSystems[ToSys_NumSys.SelectedIndex], NumSys_input.Text, 8);
+		}
+
+		private void NumSys_output_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if(NumSys_output.Text!="" && Clipboard.GetText() != NumSys_output.Text) Clipboard.SetText(NumSys_output.Text);
+		}
+	}
 }
