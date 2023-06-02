9d6a8f21df5eb36fb4ef7c7fdce9562600019c67
Yehor <yeros05040504@gmail.com>
2023-06-02 17:30:24 +0300

    Currency translator improved

---
+++ modern_calculator/Core/Currencies.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modern_calculator.Core
+{
+    internal class Currencies
+    {
+        public List<Currency> _currencies { get; set; }
+    }
+}
--- modern_calculator/MVVM/View/CurrencyTranslatorView.xaml.cs
+++ modern_calculator/MVVM/View/CurrencyTranslatorView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MaterialDesignColors;
+using modern_calculator.Core;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,117 +22,80 @@ using System.Windows.Shapes;
 
 namespace modern_calculator.MVVM.View
 {
-	/// <summary>
-	/// Interaction logic for CurrencyTranslatorView.xaml
-	/// </summary>
-	public partial class CurrencyTranslatorView : UserControl
-	{
-		private string line = "";
-		private string[] currency = { "USD", "EUR", "JPY", "UAH" };
-		public CurrencyTranslatorView()
-		{
-			InitializeComponent();
-			To_CurrTrans.SelectedIndex = 0;
-			From_CurrTrans.SelectedIndex = 3;
-			ErrorBorder_CurrTrans.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
-			WebClient wb = new WebClient();
-			try
-			{
-				line = wb.DownloadString("https://bank.gov.ua/NBU_Exchange/exchange?date=" + DateTime.Now.ToString("dd.M.yyyy"));
-			}
-			catch
-			{
-				CurrTrans_output.Text = "An error occured when processing your request";
-			}
-		}
-		private double getCurrency(string name, string xml)
-		{
-			if (!xml.Contains(name)) return 0;
-			int i = xml.IndexOf(name), b = 55;
-			while (i < xml.IndexOf(name) + b)
-			{
-				i++;
-				if (xml[i] == '0') b++;
-			}
-			string res = "";
-			while (xml[i] == '.' || Char.IsDigit(xml[i]))
-			{
-				res += xml[i];
-				i++;
-			}
-			return double.Parse(res.Replace(".", ",")) / Math.Pow(10,b-55);
-		}
-		private double Convert(string from, string to, double value)
-		{
-			if (from == to) return value;
-			if (from == "UAH") return Math.Round(value / getCurrency(to, line), 3);
-			if (to == "UAH") return Math.Round((getCurrency(from, line) * value), 3);
-			return Math.Round(value / getCurrency(to, line) * getCurrency(from, line) * value, 3);
-		}
-		private void ClearError()
-		{
-			ErrorLabel_CurrTrans.Text = "";
-			ErrorBorder_CurrTrans.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
-		}
-		private void Error(string err)
-		{
-			ErrorBorder_CurrTrans.Background = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
-			ErrorLabel_CurrTrans.Text = err;
-		}
-		private void Submit_CurrTrans_Click(object sender, RoutedEventArgs e)
-		{
-			ClearError();
+    /// <summary>
+    /// Interaction logic for CurrencyTranslatorView.xaml
+    /// </summary>
+    public partial class CurrencyTranslatorView : UserControl
+    {
+        private string[] currency = { "USD", "EUR", "JPY", "UAH" };
+        private string json = "";
+        List<Currency> currencies;
+        public CurrencyTranslatorView()
+        {
+            InitializeComponent();
+            To_CurrTrans.SelectedIndex = 0;
+            From_CurrTrans.SelectedIndex = 3;
+            ErrorBorder_CurrTrans.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+            WebClient wb = new WebClient();
+            try
+            {
+                json = wb.DownloadString("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json");
+                currencies = new JavaScriptSerializer().Deserialize<List<Currency>>(json);
+            }
+            catch
+            {
+                CurrTrans_output.Text = "An error occured when processing your request";
+            }
+        }
+        private double getCurrency(string name) => currencies.Find(el => el.cc == name).rate;
+        private double ConvertCurr(string from, string to, double value)
+        {
+            if (from == to) return value;
+            if (from == "UAH") return Math.Round(value / getCurrency(to), 3);
+            if (to == "UAH") return Math.Round(getCurrency(from) * value, 3);
+            return Math.Round(value / getCurrency(to) * getCurrency(from) * value, 3);
+        }
+        private void ClearError()
+        {
+            ErrorLabel_CurrTrans.Text = "";
+            ErrorBorder_CurrTrans.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+        }
+        private void Error(string err)
+        {
+            ErrorBorder_CurrTrans.Background = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+            ErrorLabel_CurrTrans.Text = err;
+        }
+        private void Submit_CurrTrans_Click(object sender, RoutedEventArgs e)
+        {
+            ClearError();
             if (CurrTrans_input.Text == "")
             {
-				Error("This field can not be empty");
-				return;
-			}
-			if(!Regex.IsMatch(CurrTrans_input.Text, "^[0-9,.]+$"))
+                Error("This field can not be empty");
+                return;
+            }
+            if (!Regex.IsMatch(CurrTrans_input.Text, "^[0-9,.]+$"))
             {
-				Error("Incorrect input");
-				return;
-			}
-			if (line != "")
-			{
-				CurrTrans_output.Text = Convert(currency[From_CurrTrans.SelectedIndex], currency[To_CurrTrans.SelectedIndex], double.Parse(CurrTrans_input.Text.Replace(".", ","))).ToString();
-			}
-		}
+                Error("Incorrect input");
+                return;
+            }
+            if (json != "")
+                CurrTrans_output.Text = ConvertCurr(currency[From_CurrTrans.SelectedIndex], currency[To_CurrTrans.SelectedIndex], Convert.ToDouble(CurrTrans_input.Text.Replace(",", "."))).ToString();
+        }
 
         private void Reverse_CurrTrans_Click(object sender, RoutedEventArgs e)
         {
-			ClearError();
-			if (CurrTrans_input.Text != "")
-			{
-				CurrTrans_input.Text = Convert(currency[From_CurrTrans.SelectedIndex], currency[To_CurrTrans.SelectedIndex], double.Parse(CurrTrans_input.Text.Replace(".", ","))).ToString();
-			}
+            ClearError();
+            if (CurrTrans_input.Text != "")
+                CurrTrans_input.Text = ConvertCurr(currency[From_CurrTrans.SelectedIndex], currency[To_CurrTrans.SelectedIndex], Convert.ToDouble(CurrTrans_input.Text.Replace(",", "."))).ToString();
             else
-            {
-				CurrTrans_output.Text = "";
-            }
-			int temp = From_CurrTrans.SelectedIndex;
-			From_CurrTrans.SelectedIndex = To_CurrTrans.SelectedIndex;
-			To_CurrTrans.SelectedIndex = temp;
-			if (CurrTrans_input.Text != "")
-			{
-				CurrTrans_output.Text = Convert(currency[From_CurrTrans.SelectedIndex], currency[To_CurrTrans.SelectedIndex], double.Parse(CurrTrans_input.Text.Replace(".", ","))).ToString();
-			}
-		}
-		private static bool HasNotMoreOneComma(string s)
-		{
-			int count = 0;
-			foreach (char c in s) if (c == ',' || c == '.') count++;
-			return count < 1;
-		}
-		private void CurrTrans_input_KeyDown(object sender, KeyEventArgs e)
+                CurrTrans_output.Text = "";
+            (To_CurrTrans.SelectedIndex, From_CurrTrans.SelectedIndex) = (From_CurrTrans.SelectedIndex, To_CurrTrans.SelectedIndex);
+            if (CurrTrans_input.Text != "")
+                CurrTrans_output.Text = ConvertCurr(currency[From_CurrTrans.SelectedIndex], currency[To_CurrTrans.SelectedIndex], Convert.ToDouble(CurrTrans_input.Text.Replace(",", "."))).ToString();
+        }
+        private void CurrTrans_input_KeyDown(object sender, KeyEventArgs e)
         {
-			if (e.Key == Key.OemComma || e.Key == Key.OemPeriod)
-			{
-				if (!HasNotMoreOneComma(CurrTrans_input.Text)) e.Handled = true;
-			}
-			if (CurrTrans_input.Text.Length > 26)
-			{
-				e.Handled = true;
-			}
-		}
+            e.Handled = (e.Key == Key.OemComma || e.Key == Key.OemPeriod) && CurrTrans_input.Text.Count(el => ".,".Contains(el)) > 0 || CurrTrans_input.Text.Length > 26;
+        }
     }
 }
